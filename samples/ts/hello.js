9a4eba386d3505d8e5e89955db30aadabbb651de
gopinath0332 <gopinath0332@outlook.com>
2016-08-28 23:44:12 +0530

    Updating welcome

--- samples/ts/hello.js
+++ samples/ts/hello.js
@@ -1,6 +1,19 @@
-function greeter(name) {
-    return "Hello " + name + "!!!!!!";
+var Student = (function () {
+    function Student(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + lastName;
+    }
+    return Student;
+}());
+function greeter(student) {
+    return "Hello " + student.fullName + "!!!!!!";
 }
 var user = "Mac";
 var arr = [1, 2, 4];
-document.body.innerHTML = greeter(arr);
+var person = {
+    firstName: "Mac1",
+    lastName: "Lmac"
+};
+var std = new Student("Gopi", "Rajendran");
+document.body.innerHTML = greeter(std);
--- samples/ts/hello.ts
+++ samples/ts/hello.ts
@@ -1,7 +1,26 @@
-function greeter(name: string) {
-    return "Hello " + name + "!!!!!!";
+class Student {
+    fullName: string;
+    constructor(public firstName, public lastName) {
+        this.fullName = firstName + " " + lastName;
+    }
+}
+
+
+interface Person {
+    firstName: string;
+    lastName: string;
+}
+
+function greeter(student: Student) {
+    return "Hello " + student.fullName + "!!!!!!";
 }
 
 let user = "Mac";
 let arr = [1, 2, 4];
-document.body.innerHTML = greeter(arr);
+let person = {
+    firstName: "Mac1",
+    lastName: "Lmac"
+};
+
+let std = new Student("Gopi", "Rajendran");
+document.body.innerHTML = greeter(std);
