9d8c0075b113be4d6c3243a449910881ea859511
gopinath0332 <gopinath0332@outlook.com>
2016-08-28 23:23:36 +0530

    Adding initial file commit

---
+++ samples/ts/hello.js
@@ -0,0 +1,6 @@
+function greeter(name) {
+    return "Hello " + name + "!!!!!!";
+}
+var user = "Mac";
+var arr = [1, 2, 4];
+document.body.innerHTML = greeter(arr);
---
+++ samples/ts/hello.ts
@@ -0,0 +1,7 @@
+function greeter(name: string) {
+    return "Hello " + name + "!!!!!!";
+}
+
+let user = "Mac";
+let arr = [1, 2, 4];
+document.body.innerHTML = greeter(arr);
