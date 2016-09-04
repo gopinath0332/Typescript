f22d937a15735e77ad9e3fa2bd89719a4e944510
gopinath0332 <gopinath0332@outlook.com>
2016-09-04 22:35:13 +0530

    Adding webpack watcher

--- webpack-boilerplate/server.js
+++ webpack-boilerplate/server.js
@@ -1,5 +1,14 @@
-var liveServer = require("live-server");
+var liveServer = require("live-server"),
+    webpack = require("webpack");
+var webpackConfig = require("./webpack.config");
 
+var compiler = webpack(webpackConfig);
+
+compiler.watch({
+    poll: true
+}, function(err, status) {
+
+});
 var params = {
     port: 9090,
     noBrowser: true
